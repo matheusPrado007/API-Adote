f85ec780dce7273c7d7ca4ad8fade706448bb6a0
Prado <peu_prado01@hotmail.com>
2023-07-17 02:53:01 -0300

    feat: cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const cors = require('cors');
 const routerAnimal = require('./routes/animalRouter');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
