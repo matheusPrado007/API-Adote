21f350f45d901a8b727677d90bcc3ba9731f269d
Prado <peu_prado01@hotmail.com>
2023-07-17 05:44:21 -0300

    feat: server.js

--- src/app.js
+++ src/app.js
@@ -4,13 +4,6 @@ const routerAnimal = require('./routes/animalRouter');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-
 app.use(cors());
 
 app.use(express.json());
--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const app = require('./app');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
