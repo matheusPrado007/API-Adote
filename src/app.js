245f3821a0bed997986168cc56ecc1ff9d0a0707
Prado <peu_prado01@hotmail.com>
2023-07-17 03:39:21 -0300

    feat: cors

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
-  
+
 app.use(cors());
 
 app.use(express.json());
