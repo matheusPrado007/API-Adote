67c7eba6fcf49ec33b2ae0707189ffb4ac19c6fb
Prado <peu_prado01@hotmail.com>
2023-07-18 00:43:08 -0300

    feat: server

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`API adtbackend está sendo executada na porta ${PORT}`);
